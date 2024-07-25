35d0d3f401c8f894aaad2e7007b15d71120be6ba
mcolmenares <maikincolmenares@gmail.com>
2024-07-25 17:46:16 -0400

    Instalacion de Bootstrap

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    // return view('home');
 });
 
 Route::get('/dashboard', function () {
@@ -18,3 +19,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
